9e0d7a481a3a7af9cbd966a9fb861befdd0d65af
Luces Umbrarum <gm.konsortium@yahoo.dk>
2025-07-28 20:44:30 +0200

    customization Options dialog

---
+++ Video Screen Saver/ConfigSheetController.h
@@ -0,0 +1,16 @@
+//
+//  ConfigSheetController.h
+//  Video Screen Saver
+//
+//  Created by sim on 28.07.25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ConfigSheetController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Video Screen Saver/ConfigSheetController.m
@@ -0,0 +1,22 @@
+//
+//  ConfigSheetController.m
+//  Video Screen Saver
+//
+//  Created by sim on 28.07.25.
+//
+
+#import "ConfigSheetController.h"
+
+@interface ConfigSheetController ()
+
+@end
+
+@implementation ConfigSheetController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
